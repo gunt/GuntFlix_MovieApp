98ba6039520cf66c13bf9708a0a2ae697ef83d19
gunt <4261629+gunt@users.noreply.github.com>
2019-09-05 18:10:58 -0600

    Initial commit

---
+++ testing.js
@@ -0,0 +1,9 @@
+console.log("Hello Node");
+100 + 200
+var firstString = "Hello"
+var secondString = "World"
+var thirdString = firstString + " " + secondString
+console.log(thirdString)
+
+console.log("Hello Atom Hello Vs Code Hello")
+console.log("Goodbye.")
