db8d2e67b0904a608f01b513d3ad5ef70f1fc19c
gunt <4261629+gunt@users.noreply.github.com>
2019-09-05 18:14:47 -0600

    remove testing files

--- testing.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello Node");
-100 + 200
-var firstString = "Hello"
-var secondString = "World"
-var thirdString = firstString + " " + secondString
-console.log(thirdString)
-
-console.log("Hello Atom Hello Vs Code Hello")
-console.log("Goodbye.")
