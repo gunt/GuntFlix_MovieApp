ec12ace73a87dbbd23dd2e1103fdcc0e1f3a75ff
gunt <4261629+gunt@users.noreply.github.com>
2019-09-09 17:21:38 -0600

    middleware function timestamp

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-
-//importing Express
 const express = require('express');
-
-// variable that encapsulates Express’s functionality to configure web server
 const app = express();
+
+var myLogger = function (req, res, next) {
+  console.log(req.url);
+  next();
+};
+
+var requestTime = function (req, res, next) {
+  req.requestTime = Date.now();
+  next();
+};
+
+app.use(myLogger);
+app.use(requestTime);
+
+app.get('/', function (req, res) {
+  var responseText = 'Welcome to my app!';
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  res.send(responseText);
+});
+app.get('/secreturl', function (req, res) {
+  var responseText = 'This is a secret url with super top-secret content.';
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  res.send(responseText);
+
+});
+app.listen(3000);
