1f5c41af3f0785491f38baf7d4cdc9c82fd37292
gunt <4261629+gunt@users.noreply.github.com>
2019-09-09 18:08:28 -0600

    testing morgan

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan');
+
 const app = express();
 
-let topBooks = [ {
-    title : 'Harry Potter and the Sorcerer\'s Stone',
-    author : 'J.K. Rowling'
-},
-{
-    title : 'Lord of the Rings',
-    author : 'J.R.R. Tolkien'
-},
-{
-    title : 'Twilight',
-    author : 'Stephanie Meyer'
-}
-]
+app.use(morgan('common'));
 
-// GET requests
-app.get('/', function(req, res) {
-  res.send('Welcome to my book club!')
+app.get('/', function (req, res) {
+  res.send('Welcome to my app!');
 });
-app.get('/documentation', function(req, res) {
-  res.sendFile('public/documentation.html', { root : __dirname }
-}));
-app.get('/books', function(req, res) {
-  res.json(topBooks)
+app.get('/secreturl', function (req, res) {
+  res.send('This is a secret url with super top-secret content.');
 });
 
-
-// listen for requests
-app.listen(8080, () =>
-  console.log('Your app is listening on port 8080.')
-);
+app.listen(8080);
