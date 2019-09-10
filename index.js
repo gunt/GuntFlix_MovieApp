48f9d3f3bcfd6d4069d59cb6ecdd2039790836ed
gunt <4261629+gunt@users.noreply.github.com>
2019-09-09 18:37:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+
+//Importing express
 const express = require('express'),
-  morgan = require('morgan');
+
+//Importing morgan middleware
+const morgan = require('morgan');
 
 const app = express();
 
-app.use(morgan('common'));
 
-app.get('/', function (req, res) {
-  res.send('Welcome to my app!');
-});
-app.get('/secreturl', function (req, res) {
-  res.send('This is a secret url with super top-secret content.');
+
+
+
+// GET requests
+
+app.get('/movies', function(req, res) {
+  res.json(topTenMovies)
 });
 
-app.listen(8080);
+app.get('/', function(req, res) {
+  res.send('Welcome to my book club!')
+});
+
+
+// listen for requests
+app.listen(8080, () =>
+  console.log('Your app is listening on port 8080.')
+);
