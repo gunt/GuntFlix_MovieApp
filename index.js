20d8e5d3ac279e17031225793f9b070225ef8214
gunt <4261629+gunt@users.noreply.github.com>
2019-09-14 15:18:53 -0600

    Express code to route all of the endpoints (no handler)

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ bodyParser = require("body-parser"),
 const app = express();
 
 
-app.use(bodyParser.json());
-
-
 //Importing morgan middleware
 const morgan = require('morgan');
 
@@ -30,108 +27,106 @@ let topTenMovies = [{
   },
   {
     title: 'Hackers (1995)',
-    description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
-    genres: 'Drama',
+    description: 'Hackers are blamed for making a virus that will capsize five oil tankers.',
+    genres: 'Comedy, Crime, Drama',
     director: {
-      name: 'Mel Gibson',
-      bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
-      birthYear: '1956'
+      name: 'Iain Declan Softley',
+      bio: 'Iain Declan Softley (born 28 October 1956) is an English film director, producer, and screenwriter. His films include Backbeat, Hackers, The Wings of the Dove, K-PAX, The Skeleton Key, and the BBC adaptation of Sadie Jones novel, The Outcast.',
+      birthYear: '1958'
     },
-    imageURL: 'https://www.imdb.com/title/tt0335345/mediaviewer/rm1588566272'
+    imageURL: 'https://www.imdb.com/title/tt0113243/mediaviewer/rm3325592832'
   },
   {
     title: 'The Social Network (2010)',
     description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
-    genres: 'Drama',
+    genres: 'Biography, Drama',
     director: {
-      name: 'Mel Gibson',
-      bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
-      birthYear: '1956'
+      name: 'David Fincher',
+      bio: 'David Andrew Leo Fincher (born August 28, 1962) is an American film director, film producer, television director, television producer, and music video director.',
+      birthYear: '1962'
     },
-    imageURL: 'https://www.imdb.com/title/tt0335345/mediaviewer/rm1588566272'
+    imageURL: 'https://www.imdb.com/title/tt1285016/mediaviewer/rm1054049280'
   },
   {
     title: 'TPB AFK: The Pirate Bay Away from Keyboard (2013)',
-    description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
-    genres: 'Drama',
+    description: 'An intellectual freedoms documentary based around the interpersonal triumphs, and defeats of the three main characters against the largest industry in the known universe. The media industry.',
+    genres: 'Documentary',
     director: {
-      name: 'Mel Gibson',
-      bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
-      birthYear: '1956'
+      name: 'Simon Klose',
+      bio: 'Simon Tobias Viktor Klose is a Swedish documentary and music video maker. His latest work is a documentary about The Pirate Bay called TPB AFK.',
+      birthYear: '1975'
     },
-    imageURL: 'https://www.imdb.com/title/tt0335345/mediaviewer/rm1588566272'
+    imageURL: 'https://www.imdb.com/title/tt2608732/mediaviewer/rm2467409152'
   },
   {
     title: 'The Matrix (1999)',
-    description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
-    genres: 'Drama',
+    description: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
+    genres: 'Action, Sci-Fi',
     director: {
-      name: 'Mel Gibson',
-      bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
-      birthYear: '1956'
+      name: 'The Wachowskis',
+      bio: 'Lana Wachowski and her sister Lilly Wachowski, (also known as The Wachowskis) are the duo behind ground-breaking movies such as The Matrix (1999) and Cloud Atlas (2012). Born to mother Lynne, a nurse, and father Ron, a businessman of Polish descent.',
+      birthYear: '1965'
     },
-    imageURL: 'https://www.imdb.com/title/tt0335345/mediaviewer/rm1588566272'
+    imageURL: 'https://www.imdb.com/title/tt0133093/mediaviewer/rm3441835264'
   },
   {
     title: 'The Godfather (1972)',
-    description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
-    genres: 'Drama',
+    description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+    genres: ' Crime, Drama',
     director: {
-      name: 'Mel Gibson',
-      bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
-      birthYear: '1956'
+      name: 'Francis Ford Coppola',
+      bio: 'Francis Ford Coppola was born in Detroit, Michigan, but grew up in a New York suburb in a creative, supportive Italian-American family.',
+      birthYear: '1939'
     },
-    imageURL: 'https://www.imdb.com/title/tt0335345/mediaviewer/rm1588566272'
+    imageURL: 'https://www.imdb.com/title/tt0068646/mediaviewer/rm2525403136'
   },
   {
     title: 'Gladiator (2000)',
-    description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
-    genres: 'Drama',
+    description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
+    genres: 'Action, Adventure, Drama ',
     director: {
-      name: 'Mel Gibson',
-      bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
-      birthYear: '1956'
+      name: 'Ridley Scott',
+      bio: 'Sir Ridley Scott is an English filmmaker. Following his commercial breakthrough in 1979 with the science fiction horror film Alien, further works include the neo-noir dystopian film Blade Runner, the road adventure film Thelma & Louise, the historical drama Gladiator and the science fiction film The Martian.',
+      birthYear: '1937'
     },
-    imageURL: 'https://www.imdb.com/title/tt0335345/mediaviewer/rm1588566272'
+    imageURL: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2274434560'
   },
   {
     title: 'Raiders of the Lost Ark (1981)',
-    description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
-    genres: 'Drama',
+    description: 'In 1936, archaeologist and adventurer Indiana Jones is hired by the U.S. government to find the Ark of the Covenant before Adolf Hitler Nazis can obtain its awesome powers.',
+    genres: 'Action, Adventure',
     director: {
-      name: 'Mel Gibson',
-      bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
-      birthYear: '1956'
+      name: 'Steven Spielberg',
+      bio: 'Steven Allan Spielberg is an American filmmaker. He is considered one of the founding pioneers of the New Hollywood era and one of the most popular directors and producers in film history. Spielberg started in Hollywood directing television and several minor theatrical releases.',
+      birthYear: '1946'
     },
-    imageURL: 'https://www.imdb.com/title/tt0335345/mediaviewer/rm1588566272'
+    imageURL: 'https://www.imdb.com/title/tt0082971/mediaviewer/rm556972800'
   },
   {
     title: 'The Dark Knight (2008)',
-    description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
-    genres: 'Drama',
+    description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    genres: 'Action, Crime, Drama ',
     director: {
-      name: 'Mel Gibson',
-      bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
-      birthYear: '1956'
+      name: 'Christopher Nolan',
+      bio: 'Christopher Edward Nolan, CBE is an English-American film director, screenwriter, and producer, who is known for making personal, distinctive films within the Hollywood mainstream. He has been called "one of the ultimate auteurs."',
+      birthYear: '1970'
     },
-    imageURL: 'https://www.imdb.com/title/tt0335345/mediaviewer/rm1588566272'
+    imageURL: 'https://www.imdb.com/title/tt0468569/mediaviewer/rm3419897856'
   },
   {
     title: 'War Room (2015)',
-    description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
+    description: 'A seemingly perfect family looks to fix their problems with the help of Miss Clara, an older, wiser woman.',
     genres: 'Drama',
     director: {
-      name: 'Mel Gibson',
-      bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
-      birthYear: '1956'
+      name: 'Alex Kendrick',
+      bio: 'Born in Athens, Georgia as the middle of three sons to Larry and Rhonwyn Kendrick. Grew up in Smyrna, GA and graduated from Kennesaw State University with a Bachelors degree in Communications.',
+      birthYear: '1970'
     },
-    imageURL: 'https://www.imdb.com/title/tt0335345/mediaviewer/rm1588566272'
+    imageURL: 'https://www.imdb.com/title/tt3832914/mediaviewer/rm229439232'
   }
 ]
 
-// app.get("/students", (req, res) => {
-//  res.send(“Successful GET request returning data on all the students”);
-// });
+app.use(bodyParser.json());
 
 // This function automatically routes all requests for static files // documentation
 app.use(express.static('public'));
@@ -159,51 +154,41 @@ app.get("/movies/genres/:genre", (req, res) => {
 
 //Get data about a director by name // /movies/directors/[name]
 app.get("/movies/directors/:name", (req, res) => {
-  res.send('Successful GET request returning data about director by name');
+  res.send('Successful GET request returning data about director by name.');
 });
 
 //Create a new User // /users
-app.get("/users", (req, res) => {
-  res.send('Successful GET request returning data about director by name');
-});
+app.post("/users", (req, res) => {
+  let newUser = req.body;
 
-// Adds data for a new student to our list of students.
-app.post("/students", (req, res) => {
-  let newStudent = req.body;
-
-  if (!newStudent.name) {
+  if (!newUser.username) {
     const message = "Missing name in request body";
     res.status(400).send(message);
   } else {
-    newStudent.id = uuid.v4();
-    Students.push(newStudent);
-    res.status(201).send(newStudent);
+    res.send("User added successfully.");
   }
 });
 
-
-
-
 //Update their user info (username, password, email, date of birth)
-app.get("/movies/directors/:name", (req, res) => {
-  res.send('Successful GET request returning data about director by name');
+// /users/[username]/[password]/[email]/[date_of_birth]
+app.put("/users/[username]/[password]/[email]/[date_of_birth]", (req, res) => {
+  res.send('User information updated successfully.');
 });
 
-//Add a movie to the User's favorites list
-app.get("/movies/directors/:name", (req, res) => {
-  res.send('Successful GET request returning data about director by name');
+//Add a movie to the user's favorites list // /favorites/[username]/[title]
+app.post("/favorites/[username]/[title]", (req, res) => {
+  res.send('Favorite movie by user added successfully.');
 });
 
-//Remove a movie from user's favorites list
-app.get("/movies/directors/:name", (req, res) => {
-  res.send('Successful GET request returning data about director by name');
+//Remove a movie from user's favorites list // /favorites/[username]/[title]
+app.delete("/favorites/[username]/[title]", (req, res) => {
+  res.send('Movie deleted from favorite list successfully.');
 });
 
-//Allow existing users to deregister
-app.get("/movies/directors/:name", (req, res) => {
-  res.send('Successful GET request returning data about director by name');
+//Allow existing users to deregister // /users/[username]
+app.delete("/users/[username]", (req, res) => {
+  res.send('User delete from registry successfully.');
 });
-
 
 //error-handling middleware function
 app.use(function(err, req, res, next) {
