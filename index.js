e0667211f16b21540443d194b841906e56a00fa2
gunt <4261629+gunt@users.noreply.github.com>
2019-09-14 16:00:33 -0600

    removing year to add another array element

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const morgan = require('morgan');
 //Return data (Movie title, description, genre, director, image URL,  about a single movie by title to the user
 //director (bio, birth year, death year) by name
 let topTenMovies = [{
-    title: 'The Passion of the Christ (2004)',
+    title: 'The Passion of the Christ',
     description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
     genres: 'Drama',
     director: {
@@ -26,7 +26,7 @@ let topTenMovies = [{
 
   },
   {
-    title: 'Hackers (1995)',
+    title: 'Hackers',
     description: 'Hackers are blamed for making a virus that will capsize five oil tankers.',
     genres: 'Comedy, Crime, Drama',
     director: {
@@ -37,7 +37,7 @@ let topTenMovies = [{
     imageURL: 'https://www.imdb.com/title/tt0113243/mediaviewer/rm3325592832'
   },
   {
-    title: 'The Social Network (2010)',
+    title: 'The Social Network',
     description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
     genres: 'Biography, Drama',
     director: {
@@ -48,7 +48,7 @@ let topTenMovies = [{
     imageURL: 'https://www.imdb.com/title/tt1285016/mediaviewer/rm1054049280'
   },
   {
-    title: 'TPB AFK: The Pirate Bay Away from Keyboard (2013)',
+    title: 'TPB AFK: The Pirate Bay Away from Keyboard',
     description: 'An intellectual freedoms documentary based around the interpersonal triumphs, and defeats of the three main characters against the largest industry in the known universe. The media industry.',
     genres: 'Documentary',
     director: {
@@ -59,7 +59,7 @@ let topTenMovies = [{
     imageURL: 'https://www.imdb.com/title/tt2608732/mediaviewer/rm2467409152'
   },
   {
-    title: 'The Matrix (1999)',
+    title: 'The Matrix',
     description: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
     genres: 'Action, Sci-Fi',
     director: {
@@ -70,7 +70,7 @@ let topTenMovies = [{
     imageURL: 'https://www.imdb.com/title/tt0133093/mediaviewer/rm3441835264'
   },
   {
-    title: 'The Godfather (1972)',
+    title: 'The Godfather',
     description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
     genres: ' Crime, Drama',
     director: {
@@ -81,7 +81,7 @@ let topTenMovies = [{
     imageURL: 'https://www.imdb.com/title/tt0068646/mediaviewer/rm2525403136'
   },
   {
-    title: 'Gladiator (2000)',
+    title: 'Gladiator',
     description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
     genres: 'Action, Adventure, Drama ',
     director: {
@@ -92,7 +92,7 @@ let topTenMovies = [{
     imageURL: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2274434560'
   },
   {
-    title: 'Raiders of the Lost Ark (1981)',
+    title: 'Raiders of the Lost Ark',
     description: 'In 1936, archaeologist and adventurer Indiana Jones is hired by the U.S. government to find the Ark of the Covenant before Adolf Hitler Nazis can obtain its awesome powers.',
     genres: 'Action, Adventure',
     director: {
@@ -103,7 +103,7 @@ let topTenMovies = [{
     imageURL: 'https://www.imdb.com/title/tt0082971/mediaviewer/rm556972800'
   },
   {
-    title: 'The Dark Knight (2008)',
+    title: 'The Dark Knight',
     description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
     genres: 'Action, Crime, Drama ',
     director: {
@@ -114,7 +114,7 @@ let topTenMovies = [{
     imageURL: 'https://www.imdb.com/title/tt0468569/mediaviewer/rm3419897856'
   },
   {
-    title: 'War Room (2015)',
+    title: 'War Room',
     description: 'A seemingly perfect family looks to fix their problems with the help of Miss Clara, an older, wiser woman.',
     genres: 'Drama',
     director: {
