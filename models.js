7dac38fb8a808c7ade49edc092620163f7b7ea00
gunt <4261629+gunt@users.noreply.github.com>
2019-10-08 14:18:18 +0200

    hashPassword, validatePassword,
    hashPassword function, which is what does the actual hashing of submitted passwords. The second function, validatePassword,

--- models.js
+++ models.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+
 var movieSchema = mongoose.Schema({
     Title : {type: String, required: true},
     Description : {type: String, required: true},
@@ -24,6 +26,15 @@ var movieSchema = mongoose.Schema({
    FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
   });
   
+  userSchema.statics.hashPassword = function(password) {
+    return bcrypt.hashSync(password, 10);
+  };
+  
+  userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+  };
+
+
   var Movie = mongoose.model('Movie', movieSchema);
   var User = mongoose.model('User', userSchema);
   
