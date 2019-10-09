43d49d272c709bcb05191351b63b4b3d3a440f56
gunt <4261629+gunt@users.noreply.github.com>
2019-10-09 23:28:46 +0200

    update

--- index.js
+++ index.js
@@ -238,4 +238,4 @@ app.delete('/users/:Username', passport.authenticate('jwt', {
 var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function() {
 console.log("Listening on Port 3000");
-})} ])
+})})
