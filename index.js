03bb803e49f3620b866ca240b598621aabde810c
gunt <4261629+gunt@users.noreply.github.com>
2019-10-09 19:27:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(morgan('common'));
 //local database connection
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true});
 
-//online database connection
+//online database connection fix
 mongoose.connect('mongodb+srv://MaxOctAdmin:vi82R3s2XP5VLL8G@maxoct-didgb.mongodb.net/myFlixDB?retryWrites=true&w=majority', {useNewUrlParser: true});
 
 // mongoose.connect('mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/myFlixDB?retryWrites=true', { useNewUrlParser: true });
