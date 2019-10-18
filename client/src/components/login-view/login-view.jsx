9ff13582cb1884f10cd65c5d52b293e0c9430eca
gunt <4261629+gunt@users.noreply.github.com>
2019-10-18 22:00:20 +0200

    update login-view.jsx scss include

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './login-view.scss';
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
@@ -12,27 +16,43 @@ export function LoginView(props) {
     props.onLoggedIn(username);
   };
 
+  // https://react-bootstrap.github.io/components/forms/
+  // React-bootstrap components form
   return (
-    <form>
-      <label>
-        Username:
-        <input
+    <Form.Group>
+      <Form.Group controlId='formBasicEmail'>
+        <Form.Label>Username</Form.Label>
+        <Form.Control
           type='text'
           value={username}
           onChange={e => setUsername(e.target.value)}
+          type='text'
+          placeholder='Enter username'
         />
-      </label>
-      <label>
-        Password:
-        <input
+        <Form.Text className='text-muted'></Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId='formBasicPassword'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control
           type='password'
           value={password}
           onChange={e => setPassword(e.target.value)}
+          type='email'
+          placeholder='Enter Password'
         />
-      </label>
-      <button type='button' onClick={handleSubmit}>
-        Submit
-      </button>
-    </form>
+        <Form.Text className='text-muted'></Form.Text>
+      </Form.Group>
+      <Button variant='primary' type='button'>
+        {' '}
+        onClick={handleSubmit}>Submit
+      </Button>
+      <Form.Group controlId='formNewUser'>
+        <Form.Text className='newUsers'>
+          New user? click <span onClick={() => props.onClick()}>Here</span> to
+          sign up{' '}
+        </Form.Text>
+      </Form.Group>
+    </Form.Group>
   );
 }
