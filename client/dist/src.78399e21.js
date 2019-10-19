627ea840fc554b13e05e04c05e288d0a79abf876
gunt <4261629+gunt@users.noreply.github.com>
2019-10-19 10:37:22 +0200

    update

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -39004,7 +39004,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62744" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64339" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
