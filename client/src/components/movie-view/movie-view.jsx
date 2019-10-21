db95af2611325ea40381e1c33a0cfefb3dc2a2fa
gunt <4261629+gunt@users.noreply.github.com>
2019-10-21 16:58:02 +0200

    Update movie-view.jsx

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,44 +1,39 @@
-// client/src/components/movie-view/movie-view.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
-import { MainView } from '../main-view/main-view';
-import Button from 'react-bootstrap/Button';
+import Media from 'react-bootstrap/Media';
 import './movie-view.scss';
 
 export class MovieView extends React.Component {
   constructor() {
     super();
+
     this.state = {};
   }
 
   render() {
-    const { movie, onClick } = this.props;
+    const { movie } = this.props;
 
     if (!movie) return null;
 
     return (
       <div className='movie-view'>
-        <div className='movie-title'>
-          <h2 className='label'>Title</h2>
-          <p className='value'>{this.props.movie.Title}</p>
-        </div>
-        <div className='movie-description'>
-          <h3 className='label'>Description</h3>
-          <p className='value'>{this.props.movie.Description}</p>
-        </div>
-        <div className='movie-genre'>
-          <h3 className='label'>Genre</h3>
-          <p className='value'>{this.props.movie.Genre.Name}</p>
-        </div>
-        <div className='movie-director'>
-          <h3 className='label'>Director</h3>
-          <p className='value'>{this.props.movie.Director.Name}</p>
-        </div>
-        <div className='return-button'>
-          <Button variant='primary' onClick={() => this.props.returnCallback()}>
-            Return
-          </Button>
-        </div>
+        <h1>{movie.Title}</h1>
+        <Media className='d-flex flex-column flex-md-row align-items-center'>
+          <Media.Body>
+            <h6>Genre: {movie.Genre.Name}</h6>
+            <h6>Director: {movie.Director.Name}</h6>
+            <br />
+            <h6>Description</h6>
+            <p>{movie.Description}</p>
+          </Media.Body>
+          <img
+            width={220}
+            height={326}
+            className='ml-3'
+            src={movie.ImageUrl} // image urlto fix from DATAbase
+            alt='Generic placeholder' //
+          />
+        </Media>
       </div>
     );
   }
@@ -47,13 +42,15 @@ export class MovieView extends React.Component {
 MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string,
+    ImageUrl: PropTypes.string,
     Description: PropTypes.string,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string
+    Genre: PropTypes.exact({
+      _id: PropTypes.string,
+      Name: PropTypes.string,
+      Description: PropTypes.string
     }),
     Director: PropTypes.shape({
       Name: PropTypes.string
     })
-  }).isRequired,
-  onClick: PropTypes.func.isRequired
+  }).isRequired
 };
