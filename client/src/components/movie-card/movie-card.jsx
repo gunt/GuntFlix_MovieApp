988ba8f1f9a720f1a71be55a9177adb0af5a9d04
gunt <4261629+gunt@users.noreply.github.com>
2019-11-11 17:56:34 +0100

    refactoring movie-card & director-view.jsx

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -17,7 +17,7 @@ export class MovieCard extends React.Component {
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
           <Link to={`/movies/${movie._id}`}>
-            <Button variant='link'>Open</Button>
+            <Button variant='link'>Learn More</Button>
           </Link>
         </Card.Body>
       </Card>
