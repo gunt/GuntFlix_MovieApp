4d302aff8017b3261e1bbb22ba6425988695fc70
gunt <4261629+gunt@users.noreply.github.com>
2019-11-18 12:30:16 +0100

    create the store index.jsx // import moviesApp reducers.js
    create the store using createStore and wrap your entire app in a provider that comes from React Redux

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,5 +1,9 @@
+// src/actions/actions.js
+
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_SORT_COLUMN = 'SET_SORT_COLUMN';
+export const SET_LOGGEDIN_USER = 'SET_LOGGEDIN_USER';
 
 export function setMovies(value) {
     return {
@@ -13,4 +17,18 @@ export function setFilter(value) {
         type: SET_FILTER,
         value
     };
+}
+
+export function setSortColumn(value) {
+    return {
+        type: SET_SORT_COLUMN,
+        value
+    };
+}
+
+export function setLoggedInUser(value) {
+    return {
+        type: SET_LOGGEDIN_USER,
+        value
+    }
 }
