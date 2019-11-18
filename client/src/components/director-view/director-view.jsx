1bef9c68d0f4b96f516957d001d5dcf16583947d
gunt <4261629+gunt@users.noreply.github.com>
2019-11-18 21:11:09 +0100

    Update director-view.jsx

--- client/src/components/director-view/director-view.jsx
+++ client/src/components/director-view/director-view.jsx
@@ -1,48 +1,38 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
 import './director-view.scss';
-
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export class DirectorView extends React.Component {
-  constructor() {
-    super();
+function DirectorView(props) {
+  const { movies, directorName } = props;
+  if (!movies || !movies.length) return null;
+  const director = movies.find(movie => movie.Director.Name === directorName)
+    .Director;
 
-    this.state = {};
-  }
-
-  render() {
-    const { director } = this.props;
-
-    if (!director) return null;
-
-    return (
-      <Card className='director-info' style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title className='director-info'>{director.Name}</Card.Title>
-          <Card.Title className='director-name'>{director.Name}</Card.Title>
-          <Card.Text>
-            Biography: <br />
-            <br />
-            {director.Bio}
-            <br />
-            Birth Year: {director.Birth}
-            <br />
-            Birth Year: {director.Death}
-          </Card.Text>
-
-          <div className='text-center'>
-            <Link to={`/`}>
-              <Button className='button-card' variant='info'>
-                Back
-              </Button>
-            </Link>
-          </div>
-        </Card.Body>
-      </Card>
-    );
-  }
+  return (
+    <div className='director-view'>
+      <h1 className='director'>{director.Name}</h1>
+      <h3>Biography</h3>
+      <div className='bio'>{director.Bio}</div>
+      <h3>Born</h3>
+      <div className='birth'>{director.Birth}</div>
+      <h3>Died</h3>
+      <h5 className='death'>{director.Death}</h5>
+      <Link to={'/'}>
+        <Button variant='outline-dark'>Back</Button>
+      </Link>
+    </div>
+  );
 }
+
+export default connect(({ movies }) => ({ movies }))(DirectorView);
+
+DirectorView.propTypes = {
+  Director: PropTypes.shape({
+    Name: PropTypes.string,
+    Bio: PropTypes.string,
+    Death: PropTypes.string
+  }).isRequired
+};
